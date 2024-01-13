0528d47aa0a3a195008a7d61fc7dcbad82b21239
B00140738 <B00140738@mytudublin.ie>
2024-01-13 18:05:13 +0000

    improved terrain generation
    modularised code to include Perlin Noise integration.

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ Sky()
 
 noise = PerlinNoise(octaves=2, seed=1)
 
-chunk_size = 10
+chunk_size = 8
+ground_depth = 8
+world_size = 2
 
-# Call the create_chunk function from create_chunk.py
-boxes = create_chunk(chunk_size)
+chunks = []
+
+# Generate chunks and store them in the 'chunks' list
+for i in range(world_size):
+    for j in range(world_size):
+        chunk = create_chunk(chunk_size, ground_depth, noise, position=(i * chunk_size, 0, j * chunk_size))
+        chunks.extend(chunk)
+
 
 def input(key):
-    for box in boxes:
+    for box in chunks:
         if box.hovered:
             if key == 'right mouse down':
                 new = Button(color=color.white, model='cube', position=box.position + mouse.normal, texture='textures/grass.png', parent=scene, origin_y=0.5, collider='mesh')
-                boxes.append(new)
+                chunks.append(new)
 
             if key == 'left mouse down':
-                boxes.remove(box)
+                chunks.remove(box)
                 destroy(box)
 
 app.run()
--- terrain/create_chunk.py
+++ terrain/create_chunk.py
@@ -1,13 +1,15 @@
 from perlin_noise import PerlinNoise
 from ursina import Button, color, scene
 
-def create_chunk(size):
+def create_chunk(size, ground_depth, noise, position=(0, 0, 0)):
     boxes = []
     for x in range(size):
-        for y in range(0, -size, -1):
-            for z in range(size):
-                box = Button(color=color.white, model='cube', position=(x, y, z), texture='textures/grass.png', parent=scene, origin_y=0.5, collider='mesh')
+        for z in range(size):
+            y = int(noise([(x + position[0]) / (size * 10), (z + position[2]) / (size * 10)]) * 10) + ground_depth
+            for i in range(y - ground_depth, y):
+                box = Button(color=color.white, model='cube', position=(x + position[0], i, z + position[2]), texture='textures/grass.png', parent=scene, origin_y=0.5, collider='mesh')
                 boxes.append(box)
+                
     return boxes
 
 
